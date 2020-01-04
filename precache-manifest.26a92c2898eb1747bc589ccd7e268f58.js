d2c485fd9b3e91313a794517854d47771b20aa85
osamocity7 <osamocity@gmail.com>
2020-01-04 21:24:12 +0100

    Updates

---
+++ precache-manifest.26a92c2898eb1747bc589ccd7e268f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2b77d91937ea7c371f49a3c92becfb",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e6aca696f66fb8732aea",
+    "url": "/react-gh-pages/static/css/main.f57d82be.chunk.css"
+  },
+  {
+    "revision": "df78f3a37390a8d9575b",
+    "url": "/react-gh-pages/static/js/2.45b5675f.chunk.js"
+  },
+  {
+    "revision": "e6aca696f66fb8732aea",
+    "url": "/react-gh-pages/static/js/main.28ef1981.chunk.js"
+  },
+  {
+    "revision": "1eaec6d7759a4b17fa15",
+    "url": "/react-gh-pages/static/js/runtime-main.74de6741.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
