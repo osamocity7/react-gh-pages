a3a89754a5400b76a88c8d341c924ffd27e8b9ac
osamocity7 <osamocity@gmail.com>
2020-01-05 19:41:07 +0100

    Updates

--- precache-manifest.fbe435a17351e81dc0375644067e03a2.js
+++ precache-manifest.fbe435a17351e81dc0375644067e03a2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "386f6ea079c29665600e6932d7d5e7ef",
+    "revision": "5f11e23eabec618fdc1379b88449393f",
     "url": "/react-gh-pages/index.html"
   },
   {
-    "revision": "e1f547bd4a56e49f9935",
+    "revision": "92d78a2b0fe2e05211a7",
     "url": "/react-gh-pages/static/css/main.f57d82be.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gh-pages/static/js/2.45b5675f.chunk.js"
   },
   {
-    "revision": "e1f547bd4a56e49f9935",
-    "url": "/react-gh-pages/static/js/main.ea88398b.chunk.js"
+    "revision": "92d78a2b0fe2e05211a7",
+    "url": "/react-gh-pages/static/js/main.e643378d.chunk.js"
   },
   {
     "revision": "1eaec6d7759a4b17fa15",
