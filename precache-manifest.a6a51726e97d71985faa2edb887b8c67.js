69a44729ceaa188b5946d8372ff0014a24b9ba3e
osamocity7 <osamocity@gmail.com>
2020-01-05 19:24:38 +0100

    Updates

---
+++ precache-manifest.a6a51726e97d71985faa2edb887b8c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a8e255d4947f142264cfb5f6125799",
+    "url": "/react-gh-pages/LoginForm/index.html"
+  },
+  {
+    "revision": "ef343cac829c188707e4",
+    "url": "/react-gh-pages/LoginForm/static/css/main.f57d82be.chunk.css"
+  },
+  {
+    "revision": "df78f3a37390a8d9575b",
+    "url": "/react-gh-pages/LoginForm/static/js/2.45b5675f.chunk.js"
+  },
+  {
+    "revision": "ef343cac829c188707e4",
+    "url": "/react-gh-pages/LoginForm/static/js/main.eef84ea9.chunk.js"
+  },
+  {
+    "revision": "ebfca0716627401e1c59",
+    "url": "/react-gh-pages/LoginForm/static/js/runtime-main.de202591.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/LoginForm/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.83e62e88377f483fa72206e5093a13e6.js"
+  "/react-gh-pages/LoginForm/precache-manifest.a6a51726e97d71985faa2edb887b8c67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/LoginForm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
