fef60be2c05f0f70c660de33d6166ca4b63ad96c
osamocity7 <osamocity@gmail.com>
2020-01-05 21:29:40 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.ebb94c87f8814e2390343e63776d679d.js"
+  "/react-gh-pages/precache-manifest.c1adbdbe99211719a0e9a6cc506de149.js"
 );
 
 self.addEventListener('message', (event) => {
